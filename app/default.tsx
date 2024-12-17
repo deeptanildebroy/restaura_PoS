b9611c500e650b38ce15455b4ed05a4aec20f249
Sudipta <sudiptaghosh9136@gmail.com>
2024-12-17 17:54:23 +0530

    Add default.tsx

---
+++ app/default.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const default = () => {
+  return (
+    <View>
+      <Text>default</Text>
+    </View>
+  )
+}
+
+export default default
